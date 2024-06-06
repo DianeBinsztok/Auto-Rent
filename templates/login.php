88329fe58078fb3268391cdbcdcb2fc9917a4efc
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2024-06-06 12:53:27 +0200

    commit before new branch

---
+++ templates/login.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../assets/style.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>Bonjour</h1>
+    </header>
+    <main>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
