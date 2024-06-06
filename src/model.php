b22c00fad843e3d343875caa377a15b4fa992095
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2024-06-06 13:29:56 +0200

    fetch rent sheets

--- src/model.php
+++ src/model.php
@@ -1,14 +1,56 @@
 <?php
-// Connexion à la base de données
-require "env.php";
-try {
-    $database = new PDO($db . ':host=' . $db_host . ';dbname=' . $db_name . ';charset=' . $db_charset, $db_user, $db_pw);
-    $statement = $database->query(
-        "SELECT * FROM `sheets` LIMIT 50"
-    );
 
-    $getAllSheets = $statement->fetchAll();
-} catch (Exception $e) {
-    echo ("Impossible d'accéder aux appels de loyers");
-    die('Erreur : ' . $e->getMessage());
+//Afficher tous les appels de loyer
+function getAllSheets()
+{
+    require "env.php";
+    try {
+        $database = new PDO($db . ':host=' . $db_host . ';dbname=' . $db_name . ';charset=' . $db_charset, $db_user, $db_pw);
+        $statement = $database->query(
+            "SELECT * FROM `sheets` LIMIT 50;"
+        );
+        $allSheets = $statement->fetchAll();
+        return $allSheets;
+
+    } catch (Exception $e) {
+        echo ("Impossible d'accéder aux appels de loyers");
+        die('Erreur : ' . $e->getMessage());
+    }
 }
+
+//Afficher le dernier appel de loyer
+function getLastSheet()
+{
+    require "env.php";
+    try {
+        $database = new PDO($db . ':host=' . $db_host . ';dbname=' . $db_name . ';charset=' . $db_charset, $db_user, $db_pw);
+        $statement = $database->query(
+            "SELECT * FROM `sheets` ORDER BY sheet_id DESC LIMIT 1"
+        );
+        $lastSheet = $statement->fetch();
+        return $lastSheet;
+
+    } catch (Exception $e) {
+        echo ("Impossible d'accéder aux appels de loyers");
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
+//Afficher un appel de loyer par son id
+function getSheetById($id)
+{
+    require "env.php";
+    try {
+        $database = new PDO($db . ':host=' . $db_host . ';dbname=' . $db_name . ';charset=' . $db_charset, $db_user, $db_pw);
+        $statement = $database->query(
+            "SELECT * FROM `sheets` WHERE sheet_id = " . $id . ";"
+        );
+        $sheet = $statement->fetch();
+        return $sheet;
+
+    } catch (Exception $e) {
+        echo ("Impossible d'accéder aux appels de loyers");
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
